3fcd8dd355cd81bf0be760982af59656101517ad
radishevskyi <radishevskyi10@gmail.com>
2024-11-18 23:16:07 +0200

    feat: created label

---
+++ src/shared/components/label/label.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Label = styled.span`
+  display: block;
+  border: 1px solid rgba(65, 83, 70, 25%);
+  background-color: rgba(65, 83, 70, 20%);
+  border-radius: 20px;
+  color: #979897;
+  padding: var(--spacing) var(--spacing-x2);
+  font-size: 12px;
+  letter-spacing: 3px;
+`;
