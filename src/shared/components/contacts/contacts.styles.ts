340e4ad74447add92cc40c2bd917cd84d48c0f7a
radishevskyi <138035341+radishevskyi@users.noreply.github.com>
2025-02-05 23:40:26 +0200

    Contacts (#13)
    * fix: styles
    
    * feat: added section contacts
    
    * feat: added translation
    
    * feat: added library
    
    * fix: styles
    
    * fix: warning
    
    * feat: added translation
    
    * fix: styles
    
    * fix: logic
    
    * fix: warning
    
    * feat: added images for vacancies section
    
    * feat: changed and added translation
    
    * fix: logic
    
    * fix: styles
    
    * feat: added for future page
    
    * fix: translation
    
    * fix: logic

---
+++ src/shared/components/contacts/contacts.styles.ts
@@ -0,0 +1,278 @@
+import { Container } from '@/shared/assets/styles/layout';
+import { mediaWidth } from '@/shared/assets/styles/mixins';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+
+export const SC = {
+  Section: styled.section`
+    color: var(--color-white);
+    margin-bottom: 100px;
+  `,
+
+  Content: styled(Container)`
+    display: flex;
+    flex-direction: column;
+    gap: calc(var(--spacing) * 6);
+  `,
+
+  Slider: styled(Slider)`
+    width: calc(100% + var(--spacing-x4));
+    margin-left: calc(0px - var(--spacing-x2));
+
+    .slick-dots {
+      bottom: -30px;
+      li button:before {
+        color: var(--color-white);
+        font-size: 10px;
+      }
+      li.slick-active button:before {
+        color: var(--color-accent);
+        opacity: 1;
+      }
+    }
+  `,
+
+  Cooperation: styled.div`
+    padding: 0 var(--spacing-x2);
+    display: inline-flex !important;
+    justify-content: center;
+  `,
+
+  CooperationImage: styled.img`
+    width: 174px;
+  `,
+
+  ContactsForm: styled.div`
+    display: flex;
+    gap: calc(var(--spacing) * 10);
+    border: 1px solid rgba(33, 31, 31, 1);
+    padding: 0 calc(var(--spacing) * 7);
+    flex-wrap: wrap;
+    ${mediaWidth('max', 'md')} {
+      padding: 0 var(--spacing-x4);
+      gap: calc(var(--spacing) * 6);
+    }
+    ${mediaWidth('max', 'sm')} {
+      padding: 0;
+      gap: var(--spacing-x3);
+    }
+  `,
+
+  FormBlock: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-x3);
+    padding: calc(var(--spacing) * 6) 0;
+    max-width: 670px;
+    min-width: 400px;
+    flex: 1;
+    ${mediaWidth('max', 'md')} {
+      padding: var(--spacing-x4) 0;
+      gap: var(--spacing-x2);
+    }
+    ${mediaWidth('max', 'sm')} {
+      min-width: 100%;
+      padding: var(--spacing-x4) var(--spacing-x3);
+    }
+  `,
+
+  TitleBlock: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--spacing-x2);
+  `,
+
+  Subtitle: styled.p`
+    opacity: 70%;
+    line-height: 1.5;
+    font-size: var(--font-size-medium);
+    max-width: 550px;
+    ${mediaWidth('max', 'xs')} {
+      font-size: var(--font-size-normal);
+    }
+  `,
+
+  Form: styled.div`
+    display: flex;
+    gap: var(--spacing-x4);
+    flex-wrap: wrap;
+  `,
+
+  InputWrapper: styled.div`
+    position: relative;
+    flex: 1;
+    border: 1px solid rgba(33, 31, 31, 1);
+    min-width: 300px;
+    min-height: 100%;
+    ${mediaWidth('max', 'xxs')} {
+      min-width: 220px;
+    }
+    input,
+    textarea,
+    select {
+      background: transparent;
+      padding: var(--spacing-x2);
+      color: white;
+      width: 100%;
+      min-height: 100%;
+      border: none;
+
+      &::placeholder {
+        color: white;
+      }
+    }
+
+    select {
+      padding-left: calc(var(--spacing) * 1.5);
+      cursor: pointer;
+      width: calc(100% - 5px);
+
+      option {
+        background: var(--color-black);
+        color: white;
+      }
+    }
+
+    textarea {
+      resize: none;
+      height: 100px;
+    }
+
+    .form-error {
+      color: indianred;
+      font-size: var(--font-size-thin);
+      position: absolute;
+      top: 110%;
+    }
+  `,
+
+  SendBtn: styled.button`
+    background-color: var(--color-accent);
+    color: var(--color-black);
+    min-height: 60px;
+    min-width: 240px;
+    border-radius: 100px;
+    cursor: pointer;
+    font-size: var(--font-size-medium);
+    gap: var(--spacing);
+    margin-top: var(--spacing-x4);
+    ${mediaWidth('max', 'sm')} {
+      font-size: var(--font-size-normal);
+      min-height: 50px;
+      min-width: 200px;
+    }
+    ${mediaWidth('max', 'xxs')} {
+      min-height: 50px;
+      min-width: 180px;
+    }
+  `,
+
+  LogosBlock: styled.div`
+    min-height: 100%;
+    background-color: rgba(250, 250, 250, 0.02);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: var(--spacing-x4) 0;
+    flex: 1;
+    gap: var(--spacing);
+    max-width: 360px;
+
+    ${mediaWidth('max', 'sm')} {
+      min-width: 100%;
+      flex-direction: row;
+      padding: var(--spacing-x2) 0;
+    }
+    ${mediaWidth('max', 'xs')} {
+      padding: var(--spacing) 0;
+    }
+    ${mediaWidth('max', 'xxs')} {
+      flex-direction: column;
+      padding: 0;
+    }
+  `,
+
+  FirstLogoWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 100%;
+    gap: var(--spacing);
+    padding: 0 var(--spacing-x4);
+    ${mediaWidth('max', 'md')} {
+      padding: 0 var(--spacing-x2);
+    }
+    ${mediaWidth('max', 'sm')} {
+      min-height: 100%;
+      min-width: 50%;
+    }
+    ${mediaWidth('max', 'xs')} {
+      padding: var(--spacing);
+    }
+    ${mediaWidth('max', 'xxs')} {
+      min-height: calc(60% - var(--spacing));
+      min-width: 100%;
+    }
+  `,
+
+  Logo: styled.img`
+    max-width: 230px;
+    width: 100%;
+  `,
+
+  BrigadeName: styled.div`
+    text-align: center;
+    opacity: 70%;
+
+    line-height: 1.5;
+    font-size: var(--font-size-medium);
+    font-weight: var(--font-weight-medium);
+    ${mediaWidth('max', 'sm')} {
+      font-size: var(--font-size-normal);
+      font-weight: var(--font-weight-regular);
+    }
+    ${mediaWidth('max', 'xs')} {
+      font-size: var(--font-size-small);
+      font-weight: var(--font-weight-thin);
+    }
+  `,
+
+  UnitName: styled.div`
+    text-align: center;
+    font-size: calc(var(--font-size-normal) * 1.6);
+    font-weight: var(--font-weight-semi-bold);
+    line-height: 1.5;
+
+    ${mediaWidth('max', 'sm')} {
+      font-size: calc(var(--font-size-normal) * 1.4);
+    }
+    ${mediaWidth('max', 'xs')} {
+      font-size: var(--font-size-medium);
+    }
+  `,
+
+  SecondLogoWrapper: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 100%;
+    border-top: 1px solid rgba(250, 250, 250, 0.1);
+    padding-top: calc(var(--spacing) * 10);
+    margin: auto;
+
+    ${mediaWidth('max', 'sm')} {
+      border-top: none;
+      min-height: 100%;
+      min-width: 50%;
+      padding-top: 0;
+    }
+    ${mediaWidth('max', 'xxs')} {
+      border-top: 1px solid rgba(250, 250, 250, 0.1);
+      min-height: calc(40% - var(--spacing-x2));
+      min-width: 100%;
+      padding: var(--spacing) 0;
+    }
+  `,
+};
---
+++ src/shared/components/contacts/contacts.tsx
@@ -0,0 +1,157 @@
+import { Label, Title } from '@/shared/assets/styles/layout';
+import useTranslation from 'next-translate/useTranslation';
+import { SC } from './contacts.styles';
+import {
+  contactsSliderSettings,
+  cooperationArray,
+  initialValues,
+} from './contacts.data';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import * as Yup from 'yup';
+import { vacanciesArray } from '@/modules/home/vacancies/vacancies.data';
+
+function Contacts() {
+  const { t } = useTranslation('contacts');
+  const { t: tV } = useTranslation('vacancies');
+
+  const validationSchema = Yup.object().shape({
+    fullName: Yup.string()
+      .matches(
+        /^[A-Za-zА-Яа-яЁёІіЇїЄєŁłĄąĆćĘęŃńÓóŚśŹźŻż\s]+$/,
+        t('errors.invalidFullName'),
+      )
+      .required(t('errors.fullName')),
+    email: Yup.string()
+      .email(t('errors.invalidEmail'))
+      .required(t('errors.email')),
+    phone: Yup.string()
+      .matches(/^\+380\d{9}$/, t('errors.phoneFormat'))
+      .required(t('errors.phone')),
+    vacancy: Yup.string().required(t('errors.vacancy')),
+    message: Yup.string()
+      .max(500, t('errors.messageTooLong'))
+      .required(t('errors.message')),
+  });
+
+  const handleSubmit = (values: any) => {
+    alert(t('successMessage'));
+  };
+
+  return (
+    <SC.Section>
+      <SC.Content>
+        <SC.Slider {...contactsSliderSettings}>
+          {cooperationArray.map((item) => (
+            <SC.Cooperation key={item.id}>
+              <SC.CooperationImage src={item.src} />
+            </SC.Cooperation>
+          ))}
+        </SC.Slider>
+
+        <SC.ContactsForm>
+          <SC.FormBlock>
+            <SC.TitleBlock>
+              <Label>{t('label')}</Label>
+              <Title.Span>{t('title')}</Title.Span>
+              <SC.Subtitle>{t('text')}</SC.Subtitle>
+            </SC.TitleBlock>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <SC.Form>
+                    <SC.InputWrapper>
+                      <Field
+                        name="fullName"
+                        type="text"
+                        placeholder={t('placeholders.fullName')}
+                      />
+                      <ErrorMessage
+                        name="fullName"
+                        component="div"
+                        className="form-error"
+                      />
+                    </SC.InputWrapper>
+
+                    <SC.InputWrapper>
+                      <Field
+                        name="email"
+                        type="email"
+                        placeholder={t('placeholders.email')}
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="form-error"
+                      />
+                    </SC.InputWrapper>
+
+                    <SC.InputWrapper>
+                      <Field
+                        name="phone"
+                        type="tel"
+                        placeholder={t('placeholders.phone')}
+                      />
+                      <ErrorMessage
+                        name="phone"
+                        component="div"
+                        className="form-error"
+                      />
+                    </SC.InputWrapper>
+
+                    <SC.InputWrapper>
+                      <Field as="select" name="vacancy">
+                        <option value="">{t('placeholders.vacancy')}</option>
+                        {vacanciesArray.map((vacancy) => (
+                          <option key={vacancy.id} value={vacancy.id}>
+                            {tV(vacancy.militaryRank)}
+                          </option>
+                        ))}
+                      </Field>
+                      <ErrorMessage
+                        name="vacancy"
+                        component="div"
+                        className="form-error"
+                      />
+                    </SC.InputWrapper>
+
+                    <SC.InputWrapper>
+                      <Field
+                        name="message"
+                        as="textarea"
+                        placeholder={t('placeholders.message')}
+                      />
+                      <ErrorMessage
+                        name="message"
+                        component="div"
+                        className="form-error"
+                      />
+                    </SC.InputWrapper>
+                  </SC.Form>
+                  <SC.SendBtn type="submit" disabled={isSubmitting}>
+                    {t('sendBtn')}
+                  </SC.SendBtn>
+                </Form>
+              )}
+            </Formik>
+          </SC.FormBlock>
+          <SC.LogosBlock>
+            <SC.FirstLogoWrapper>
+              <SC.Logo src="/img/logo-103-OBR.png" />
+              <SC.BrigadeName>{t('brigadeName')}</SC.BrigadeName>
+              <SC.UnitName>{t('unitName')}</SC.UnitName>
+            </SC.FirstLogoWrapper>
+            <SC.SecondLogoWrapper>
+              <SC.Logo src="/img/logo-krila-do-pecla.png" />
+            </SC.SecondLogoWrapper>
+          </SC.LogosBlock>
+        </SC.ContactsForm>
+      </SC.Content>
+    </SC.Section>
+  );
+}
+
+export default Contacts;
