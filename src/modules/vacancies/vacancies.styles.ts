123446e2986ef156d93a0af60a5935d485b1068a
radishevskyi <radishevskyi10@gmail.com>
2025-02-07 20:42:33 +0200

    feat: added vacancies page

---
+++ src/modules/vacancies/vacancies.styles.ts
@@ -0,0 +1,79 @@
+import { Container, Title } from '@/shared/assets/styles/layout';
+import { mediaWidth } from '@/shared/assets/styles/mixins';
+import { HEADER_HEIGHT } from '@/shared/components/header/header.styles';
+import styled from 'styled-components';
+
+export const cardHeight = {
+  lg: 400,
+  xs: 300,
+  xxs: 200,
+};
+
+export const SC = {
+  Section: styled.section`
+    color: var(--color-white);
+    margin-bottom: 100px;
+    ${mediaWidth('max', 'lg')} {
+      margin-bottom: 70px;
+    }
+    ${mediaWidth('max', 'md')} {
+      margin-bottom: 50px;
+    }
+    ${mediaWidth('max', 'sm')} {
+      margin-bottom: 30px;
+    }
+  `,
+
+  Content: styled(Container)`
+    padding-top: calc(${HEADER_HEIGHT}px + var(--spacing-x4));
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-x2);
+  `,
+
+  TitleBlock: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--spacing-x2);
+    margin-bottom: var(--spacing-x4);
+  `,
+
+  Title: styled(Title.Span)`
+    max-width: 600px;
+    ${mediaWidth('max', 'xs')} {
+      max-width: 400px;
+    }
+  `,
+
+  Subtitle: styled.p`
+    opacity: 70%;
+    line-height: 1.5;
+    font-size: var(--font-size-medium);
+    max-width: 630px;
+    ${mediaWidth('max', 'xxs')} {
+      font-size: var(--font-size-normal);
+    }
+  `,
+
+  Vacancies: styled.div`
+    display: flex;
+    gap: calc(var(--spacing) * 6);
+    flex-wrap: wrap;
+  `,
+
+  CardWrapper: styled.div`
+    min-height: ${cardHeight.lg}px;
+    min-width: 400px;
+    flex: 1;
+
+    ${mediaWidth('max', 'xs')} {
+      min-width: 250px;
+      min-height: ${cardHeight.xs}px;
+    }
+    ${mediaWidth('max', 'xxs')} {
+      min-height: ${cardHeight.xxs}px;
+    }
+  `,
+};
---
+++ src/modules/vacancies/vacancies.tsx
@@ -0,0 +1,30 @@
+import { Label } from '@/shared/assets/styles/layout';
+import useTranslation from 'next-translate/useTranslation';
+import { SC } from './vacancies.styles';
+import { vacanciesArray } from '@/shared/vacancies-card/vacancies.mock';
+import VacancyCard from '@/shared/vacancies-card';
+
+function Vacancies() {
+  const { t } = useTranslation('vacancies');
+
+  return (
+    <SC.Section>
+      <SC.Content>
+        <SC.TitleBlock>
+          <Label>{t('label')}</Label>
+          <SC.Title>{t('title')}</SC.Title>
+          <SC.Subtitle>{t('subtitle')}</SC.Subtitle>
+        </SC.TitleBlock>
+        <SC.Vacancies>
+          {vacanciesArray.map((vacancy) => (
+            <SC.CardWrapper key={vacancy.id}>
+              <VacancyCard vacancy={vacancy} />
+            </SC.CardWrapper>
+          ))}
+        </SC.Vacancies>
+      </SC.Content>
+    </SC.Section>
+  );
+}
+
+export default Vacancies;
